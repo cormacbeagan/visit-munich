0f785312285e01915cbd42fbd221dc2d37e2dcc5
cormacbeagan <cormacbeagan@gmail.com>
2021-03-14 02:14:54 +0100

    removed box scaling

--- src/components/Styles/InfoBoxStyles.js
+++ src/components/Styles/InfoBoxStyles.js
@@ -8,17 +8,6 @@ const InfoBoxStyles = styled.div`
   width: 320px;
   display: block;
   transition: left 400ms cubic-bezier(0.5, 1.71, 0.54, 0.89);
-  &:focus {
-    box-shadow: none;
-  }
-  @media only screen and (max-width: 480px) {
-    transform: scale(0.95);
-  }
-  @media only screen and (max-width: 380px) {
-    top: 0px;
-    transform: scale(0.7);
-    left: ${props => (props.slideIn ? '-50px' : '-350px')};
-  }
 `;
 
 export default InfoBoxStyles;
