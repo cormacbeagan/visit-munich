62feea674de268965ff5b60cf1e9be978d8d8af2
cormacbeagan <cormacbeagan@gmail.com>
2021-03-16 00:47:53 +0100

    last inline styles replaced

--- src/components/home/blogDisplay.js
+++ src/components/home/blogDisplay.js
@@ -4,6 +4,7 @@ import advancedFormat from 'dayjs/plugin/advancedFormat';
 import HomeEntry from './homeEntry';
 import BoxWrapper from '../universal/boxWrapper';
 import Loading from '../universal/loading';
+import { FlexColumn, FlexRow, InfoBox } from '../Styles/EditStyles';
 dayjs.extend(advancedFormat);
 
 function BlogDisplay(props) {
@@ -11,46 +12,46 @@ function BlogDisplay(props) {
   if (blog) {
     return (
       <div>
-        <div style={row}>
+        <FlexRow>
           <BoxWrapper>
             <HomeEntry data={{ ...blog, id: 1 }} handleEditMode={handleEdit} />
           </BoxWrapper>
-        </div>
-        <div style={row}>
-          <div style={marg}>
-            <p style={lowlight}>
-              Link URL: <span style={highlight}>{blog.link}</span>
+        </FlexRow>
+        <FlexColumn>
+          <InfoBox>
+            <p>
+              Link URL: <span>{blog.link}</span>
             </p>
-            <p style={lowlight}>
-              Link Text: <span style={highlight}>{blog.linkText}</span>
+            <p>
+              Link Text: <span>{blog.linkText}</span>
             </p>
-            <p style={lowlight}>
-              Position: <span style={highlight}>{Number(blog.rank) + 1}</span>
+          </InfoBox>
+          <InfoBox>
+            <p>
+              Position: <span>{Number(blog.rank) + 1}</span>
             </p>
-          </div>
-          <div style={marg}>
-            <p style={lowlight}>
+            <p>
               Posted by:{' '}
-              <span
-                style={highlight}
-              >{`${blog.authorFirstName} ${blog.authorLastName}`}</span>
+              <span>{`${blog.authorFirstName} ${blog.authorLastName}`}</span>
             </p>
-            <p style={lowlight}>
+          </InfoBox>
+          <InfoBox>
+            <p>
               Posted:{' '}
-              <span style={highlight}>
+              <span>
                 {dayjs(blog.createdAt.toDate()).format('ddd Do MMM YYYY')}
               </span>
             </p>
             {blog.updatedAt && (
-              <p style={lowlight}>
+              <p>
                 Last updated:{' '}
-                <span style={highlight}>
+                <span>
                   {dayjs(blog.updatedAt.toDate()).format('ddd Do MMM YYYY')}
                 </span>
               </p>
             )}
-          </div>
-        </div>
+          </InfoBox>
+        </FlexColumn>
       </div>
     );
   } else {
@@ -64,30 +65,3 @@ BlogDisplay.propTypes = {
 };
 
 export default BlogDisplay;
-
-const lowlight = {
-  color: '#dfbaaa',
-  fontWeight: '600',
-};
-const highlight = {
-  color: '#cecbcb',
-};
-const row = {
-  background: '#464646',
-  margin: '50px auto',
-  padding: '20px',
-  maxWidth: '800px',
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  boxShadow: '0 30px 50px rgba(0, 0, 0, 0.3)',
-  borderRadius: '5px',
-};
-
-const marg = {
-  margin: '10px',
-  maxWidth: '300px',
-  overflow: 'hidden',
-  flexGrow: '1',
-};
