5d337f1631ec1d05b55352a04e52ec8d867a447d
cormacbeagan <cormacbeagan@gmail.com>
2021-03-19 19:25:41 +0100

    redux devtools

--- src/index.js
+++ src/index.js
@@ -22,9 +22,7 @@ import { useSelector } from 'react-redux';
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
     : compose;
 
 const enhancer = composeEnhancers(
